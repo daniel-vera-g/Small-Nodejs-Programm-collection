1fd1515b1e65d4aaf925e82480475da2cf4acf63
DanielVG <danielveragi@gmail.com>
2018-01-17 15:16:19 +0100

    Improved the code quality of the filteredLs excercise. TODO is improving the code quality of all excercises before.

--- filteredLs.js
+++ filteredLs.js
@@ -10,16 +10,36 @@ const path = require('path');
 
 //get the path to the directory and the extension of the file
 const pathToDir = process.argv[2];
-
 const extension = process.argv[3];
 
-//read the file of the directory and save the save the in the callback function
-fs.readdir(pathToDir, function (err, list) {
-    // loop through each file in the folder and check if the extension match with the provided one
-    list.forEach(function (file){
-        if (path.extname(file) === '.' + extension) {
-            console.log(file);
-        }
+/* 
+Get Files from directory that have extension
+@param Path to the directory
+@param extension
+@param callback function
+ */
+var getFiles = function (dir, ext, callback ) {
+    //  use the readdir function to the the right files
+    fs.readdir(dir, function (err, list) {
+        if (err) return console.log(err);
+    
+        // use the filter function to extract the right files
+        list = list.filter(function(file){
+             return path.extname(file) == '.' + ext;
+            })
+        // call the callback function when the list is sorted
+        callback(null, list);
     })
+}
 
-})
+/*
+Function call to get the files with the respectively extension
+ */
+getFiles(pathToDir, extension, function(err, list){
+    if (err) console.log(err);
+    
+    // loop through the results of the function
+    list.forEach(function(file){
+        console.log(file);
+    });
+});
