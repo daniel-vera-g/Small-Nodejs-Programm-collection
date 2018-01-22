11f8c2b13383d323ed9062a91a7ead1d33ccd288
DanielVG <danielveragi@gmail.com>
2018-01-22 19:04:17 +0100

    made http client

---
+++ httpClient.js
@@ -0,0 +1,23 @@
+/**
+ * A program that performs an HTTP GET request to a URL provided to you as the first command-line argument.
+  Write the String contents of each "data" event from the response to a new line on the console (stdout). 
+ */
+
+ var http = require('http');
+
+//  set the standart input to the url
+const url = process.argv[2];
+
+
+let httpRequest = http.get(url, function(response){
+    // change encoding to utfs8
+    response.setEncoding('utf8');
+    // log the data to the output 
+    response.on('data', function(data){
+        console.log(data);
+    });
+    // log errors
+    response.on('error', function(error){
+        console.log(error);
+    })
+})
