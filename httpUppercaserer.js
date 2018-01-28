e38d7f26d61739554764f693e9ea9ebbb695e8f7
DanielVG <danielveragi@gmail.com>
2018-01-28 10:25:38 +0100

    httpUpperCaser

---
+++ httpUppercaserer.js
@@ -0,0 +1,25 @@
+/**
+ * Write an HTTP server that receives only POST requests and converts  
+  incoming POST body characters to upper-case and returns it to the client.  
+ */
+const http = require("http");
+const fs = require("fs");
+const map = require('through2-map');
+
+const port = process.argv[2];
+
+http.createServer(function(req, res) {
+  
+  if (req.method == 'POST') {
+    // pipe the req to the map function
+    req.pipe(map(function(chunk){
+      // convert chuk form repsonse to string and Upppercase
+      return chunk.toString().toUpperCase();
+    })).pipe(res);
+  }else{
+    return res.end('Send me a Post request\n');
+  }
+
+  }).listen(port, () => {
+    console.log("Server started on Port " + port);
+  });
