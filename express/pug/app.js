cdbef9801b336609d14518ac50e320f4aa5ab313
DanielVG <danielveragi@gmail.com>
2018-01-29 19:39:47 +0100

    pug templating engine

--- express/pug/app.js
+++ express/pug/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const pug = require('pug');
 const port = process.argv[2];
 
+// set templating engine
+app.set('view engine', 'pug');
+// set views
+app.set('views', process.argv[3] || path.join(__dirname, 'templates'));
 // routes for /home
 app.get("/home", (req, res) => {
-  res.end("Hello World!");
+  res.render("index", { date: new Date().toDateString() });
 });
 
 app.listen(port);
