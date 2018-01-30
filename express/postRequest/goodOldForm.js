fd8fa6e87c7a9b6b4cd6db69da66b04b48ead89e
DanielVG <danielveragi@gmail.com>
2018-01-30 15:49:15 +0100

    responding to form

--- express/postRequest/goodOldForm.js
+++ express/postRequest/goodOldForm.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const bodyparser = require('body-parser');
+
 const app = express();
+app.use(bodyparser.urlencoded({extended: false}));
+
 const port = process.argv[2];
 
+app.post('/form', (req, res) => {
+    let responseValue = req.body.str.split('').reverse().join('');
+    res.end(responseValue)
+});
 
 app.listen(port);
