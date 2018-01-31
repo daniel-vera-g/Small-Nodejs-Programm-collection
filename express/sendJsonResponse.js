bcdf0501e9979280fc01356ba2e12ea6438161c9
DanielVG <danielveragi@gmail.com>
2018-01-31 15:07:59 +0100

    send json back to the server

---
+++ express/sendJsonResponse.js
@@ -0,0 +1,20 @@
+ /**
+  * process the data from the URL query string (urlencoded). 
+  */
+const express = require('express');
+
+const app = express();
+const port = Number(process.argv[2]);
+
+// get req handler
+app.get('/search', (req, res) => {
+  let object = req.query;
+  // send object
+  res.send(object);
+})
+
+try{
+  app.listen(port);
+} catch(err){
+  console.error(err.message);
+}
