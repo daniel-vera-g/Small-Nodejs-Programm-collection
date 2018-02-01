b9deee597791ec049df0bc2c71d3b74c80fe6bf8
DanielVG <danielveragi@gmail.com>
2018-02-01 14:53:00 +0100

    some code improvements

--- mongoDB/findDomcuments.js
+++ mongoDB/findDomcuments.js
@@ -1,42 +1,29 @@
-// const mongo = require('mongodb').MongoClient;
-
-// // age to filter
-// const reqAge = parseInt(process.argv[2]);
-
-// // connect to database
-// mongo.connect('mongodb://localhost:27017/learnyoumongo', (err, database) => {
-//     if (err) throw err;
-//     // get collection
-//     let parrots = database.collection('parrots');
-//     // find values that where age is bigger than the requiredd age
-//     parrots.find({
-//         age: {
-//             $gt: +reqAge
-//         }
-//     }).toArray((err, docs) => {
-//         if (err) throw err;
-//         console.log(docs);
-//         database.close();
-//     })
-// })
-
 var mongo = require("mongodb").MongoClient;
+
+// age to filter
 var age = process.argv[2];
 
+// url for request
 var url = "mongodb://localhost:27017/learnyoumongo";
 
-mongo.connect(url, function(err, database) {
+// connect to database
+mongo.connect(url, (err, database) => {
   if (err) throw err;
+
+  // get database
   var myDb = database.db("learnyoumongo");
-  let parrots = myDb.collection('parrots');
+
+  // get collection
+  let parrots = myDb.collection("parrots");
+
+  // find values that where age is bigger than the requiredd age
   parrots.find({
-      age: {
-        $gt: +age
-      }
-    })
-    .toArray(function(err, docs) {
-      if (err) throw err;
-      console.log(docs);
-      db.close();
-    });
+    age: {
+      $gt: +age
+    }
+  }).toArray((err,docs) => {
+    if (err) throw err;
+    console.log(docs);
+    db.close();
+  });
 });
