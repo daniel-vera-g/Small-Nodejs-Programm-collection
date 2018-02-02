46b7fd0f411d8362c8a52d89d7ea1ce20d39dd87
DanielVG <danielveragi@gmail.com>
2018-02-02 16:48:20 +0100

    count & return greater data

---
+++ mongoDB/count.js
@@ -0,0 +1,34 @@
+/**
+ * Count number of documents with a certain criteria
+ */
+var MongoClient = require("mongodb").MongoClient;
+
+// number to compare
+const number = process.argv[2];
+//database name
+const dbName = "learnyoumongo";
+// collection name
+const collectionName = "parrots";
+
+// url for the database
+const url = "mongodb://localhost:27017/learnyoumongo";
+
+// connect to database
+MongoClient.connect(url, (err, database) => {
+  if (err) throw err;
+
+  // get database
+  var db = database.db(dbName);
+
+  // get collection
+  const parrots = db.collection(collectionName);
+
+//   count data with certain criteria
+    parrots.count({
+        age: { $gt: +number}
+    }, (err,count) => {
+        if (err) throw err;
+        console.log(count);
+        db.close();
+    })
+});
